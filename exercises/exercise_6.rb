94c18ec4e8ced2fce703dd84e47df6a741bac8ce
Lilian Shih <l.shih@outlook.com>
2017-12-05 21:36:36 +0000

    finished ex 6, modified emps and store files under lib

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,8 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Roe", last_name: "Virt", hourly_rate: 60)
+@store1.employees.create(first_name: "Joe", last_name: "Dirt", hourly_rate: 90)
+@store2.employees.create(first_name: "Moe", last_name: "Wirt", hourly_rate: 55)
+@store2.employees.create(first_name: "Doe", last_name: "Zirt", hourly_rate: 25)
+@store2.employees.create(first_name: "Soe", last_name: "Mirt", hourly_rate: 80)
