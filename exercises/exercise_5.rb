016ebb3eaae95ab7aa9862546a22bb178b88b954
Lilian Shih <l.shih@outlook.com>
2017-12-05 21:27:45 +0000

    finished ex 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+
+
+# Output the number of stores that are generating $1M or more in annual sales. 
+# Hint: Chain together where and size (or count) Active Record methods.
+
+
+puts "This company has a total annual revenue of #{Store.sum(:annual_revenue)}"
+puts "Their average revenue per store is #{Store.average(:annual_revenue)}"
+puts "There are #{Store.where("annual_revenue >= ?", 1000000).count} stores that have met or exceeded $1M in annual revenue"
